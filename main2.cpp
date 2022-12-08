c5b0af790da3a824ac965f47e6301ef033bb1e53
d3adl1n3 <ikateshov@gmail.com>
2022-12-08 13:28:31 +0700

    function declared, add float support

--- main2.cpp
+++ main2.cpp
@@ -4,15 +4,31 @@
 
 using namespace std;
 
-int rand(int min, int max) {
+float rand(int min, int max) {
     return 1;
 }
 
-int** getSquareRandMatrix(int N) {
-    int **arr;
-    arr = (int **)calloc(N, sizeof(*arr));
+float** get_I_matrix(int N) {
+    float **arr;
+    arr = (float **)calloc(N, sizeof(*arr));
     for (int i = 0; i < N; ++i)
-        arr[i] = (int*)calloc(N, sizeof(*arr[i]));
+        arr[i] = (float*)calloc(N, sizeof(*arr[i]));
+       
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            arr[i][j] = 1;
+        }
+    }
+    
+    return arr;
+}
+
+
+float** get_rand_matrix(int N) {
+    float **arr;
+    arr = (float **)calloc(N, sizeof(*arr));
+    for (int i = 0; i < N; ++i)
+        arr[i] = (float*)calloc(N, sizeof(*arr[i]));
        
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -37,23 +53,23 @@ void add_arrays_intrins(int* a, int* b, int* target, int size) {
     }
 }
 
-void add_matrix_intrins(int** a, int** b, int** target, int size) {
+void add_matrix_intrins(float** a, float** b, float** target, int size) {
     for(int j=0; j<size; j++) {
         for(int i=0; i<size; i+=4) {
             /* Load data into NEON register */
-            int32x4_t av = vld1q_s32(&(a[j][i]));
-            int32x4_t bv = vld1q_s32(&(b[j][i]));
+            float32x4_t av = vld1q_f32(&(a[j][i]));
+            float32x4_t bv = vld1q_f32(&(b[j][i]));
 
             /* Perform the addition */
-            int32x4_t targetv = vaddq_s32(av, bv);
+            int32x4_t targetv = vaddq_f32(av, bv);
 
             /* Store the result */
-            vst1q_s32(&(target[i][j]), targetv);
+            vst1q_f32(&(target[i][j]), targetv);
         }
     }
 }
 
-void add_matrix(int** a, int** b, int** target, int size) {
+void add_matrix(float** a, float** b, float** target, int size) {
     for (int j=0; j<size; j++) {
         for (int i=0; i<size; i++) {
             int av = a[j][i];
@@ -64,29 +80,48 @@ void add_matrix(int** a, int** b, int** target, int size) {
     }
 }
 
+void sub_matrix() {}
+void sub_matrix_intrins() {}
+
+void div_matrix() {}
+void div_matrix_intrins() {}
+
+void mult_matrix() {}
+void mult_matrix_intrins() {}
+
+void calculate_T_matrix() {}
+void calculate_T_matrix_intrins() {}
+
+void calculate_B_matrix() {}
+void calculate_B_matrix_intrins() {}
+
+void calculate_R_matrix() {}
+void calculate_R_matrix_intrins() {}
+
+
 int main(int argc, const char * argv[]) {
     int N = 10000;
     struct timespec start, end;
     
-    int **a;
-    a = getSquareRandMatrix(N);
-    a = getSquareRandMatrix(N);
-    int **b;
-    b = getSquareRandMatrix(N);
-    int **res;
-    res = getSquareRandMatrix(N);
+    float **a;
+    a = get_rand_matrix(N);
+    a = get_rand_matrix(N);
+    float **b;
+    b = get_rand_matrix(N);
+    float **res;
+    res = get_rand_matrix(N);
     
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     add_matrix(a, b, res, N);
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     printf("Time taken to calculate with intrinsics is %lf sec.\n", end.tv_sec-start.tv_sec + 0.000000001*(end.tv_nsec-start.tv_nsec));
-    printf("%d\n", res[0][0]);
+    printf("%f\n", res[0][0]);
     
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     add_matrix_intrins(a, b, res, N);
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     printf("Time taken to calculate with intrinsics is %lf sec.\n", end.tv_sec-start.tv_sec + 0.000000001*(end.tv_nsec-start.tv_nsec));
-    printf("%d\n", res[0][0]);
+    printf("%f\n", res[0][0]);
     
     for (int i = 0; i < N; ++i) {
         free(a[i]);
